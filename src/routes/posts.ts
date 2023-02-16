bcf0d8a10e6d112506df48fe985d967961e7c53a
Stav Byevsky <stav134679@gmail.com>
2023-02-16 08:08:18 +0200

    add post posts, get posts by topic

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,10 +1,35 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 import express, { Request, Response, Router } from 'express';
 
-const prisma = new PrismaClient()
-export const postsRouter: Router = express.Router() 
+const prisma = new PrismaClient();
+export const postsRouter: Router = express.Router();
 
 postsRouter.get('/', async (req: Request, res: Response) => {
-    const posts = await prisma.post.findMany();
-    res.send(posts)
-})
+  const posts = await prisma.post.findMany();
+  res.send(posts);
+});
+
+postsRouter.get('/byTopic', async (req: Request, res: Response) => {
+  if (!req.query.topic) {
+    res.status(400).send('topic parameter is missing');
+    return;
+  }
+  const topidId = parseInt(req.query.topic as string);
+
+  const posts = await prisma.post.findMany({
+    where: { topic_id: topidId },
+    include: { user: { select: { user_id: true, picture: true } } },
+  });
+  res.send(posts);
+});
+
+postsRouter.post('/', async (req: Request, res: Response) => {
+  const post = req.body;
+
+  try {
+    await prisma.post.create({ data: post });
+    res.status(200).send('success');
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
--- src/routes/topics.ts
+++ src/routes/topics.ts
@@ -1,10 +1,10 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 import express, { Request, Response, Router } from 'express';
 
-const prisma = new PrismaClient()
-export const topicsRouter: Router = express.Router() 
+const prisma = new PrismaClient();
+export const topicsRouter: Router = express.Router();
 
 topicsRouter.get('/', async (req: Request, res: Response) => {
-    const topics = await prisma.topics.findMany();
-    res.send(topics)
-})
+  const topics = await prisma.topics.findMany();
+  res.json(topics);
+});
